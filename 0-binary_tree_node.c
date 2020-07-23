b061d417b8bd36e14399295d20ea30076018f24c
pasignature <pasignature@gmail.com>
2020-07-23 22:39:36 +0000

    updates

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,15 +9,13 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	if (!newnode)
 		return (NULL);
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	return (new_node);
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
 }
